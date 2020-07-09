fa3f59f83276a6a202d2d2feea32efd3972c3d52
lattanzioB <brunoj.lattanzio@gmail.com>
2020-07-09 19:49:29 -0300

    adding notification behaviour

---
+++ NotificationApi/src/notify.js
@@ -0,0 +1,29 @@
+
+
+class Notification {
+
+    constructor(){
+        this._suscriptors = [];
+    }
+
+    addSuscriptor(emailToSuscribe, artistIDToSuscribe){
+        this._suscriptors.push(
+            {
+                email: emailToSuscribe,
+                artistId: artistIDToSuscribe
+
+            }
+        )
+    }
+
+    removeSuscriptor(emailToDesuscribe, artistIDToDesuscribe){
+        this._suscriptors = this._suscriptors.filter(
+            item => item.email !== emailToDesuscribe && item.artistId !== artistIDToDesuscribe)
+    }
+
+    removeArtist(artistIDToDesuscribe){
+        this._suscriptors = this._suscriptors.filter(item => item.artistId !== artistIDToDesuscribe)
+    }
+
+    
+}
