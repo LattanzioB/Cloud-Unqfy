224503da1203d8bcec43a59a8df26f529fbf6d09
LattanzioB <taato.lat@gmail.com>
2020-07-22 20:44:24 -0300

    periodic service status check

--- Monitoreo-Slack/src/service.js
+++ Monitoreo-Slack/src/service.js
@@ -1,9 +1,11 @@
 let express = require("express"); // import express
 const rp = require('request-promise');
+const cron = require('node-cron');
 let app = express(); // define our app using express
 let router = express.Router();
 let bodyParser = require("body-parser");
 let {Monitoreo} = require("./monitoreo");
+let {SlackClient} = require("./postMessage");
 const {
     NotFoundError,
     NotFoundRelatedError,
@@ -24,7 +26,26 @@ try {
     console.log(e);
   }
 
-const monitoreo = new Monitoreo()
+const monitoreo = new Monitoreo() 
+const slackClient = new SlackClient()
+
+
+let task = cron.schedule('* * * * *', () => {
+  let result = {
+    "unqfy" : "Service offline",
+    "loggly" : "Service offline",
+    "notification" : "Service offline"
+  }
+  let promise = Promise.all([monitoreo.stateOfUnqfy(),monitoreo.stateOfLoggly() , monitoreo.stateOfNotification()])
+  promise.then((ress) => {
+    result.unqfy = ress[0]
+    result.loggly = ress[1]
+    result.notification = ress[2]
+    if(result.unqfy == "Service Off" || result.loggly == "Service Off" || result.notification == "Service Off"){
+      slackClient.postMessage(`Unqyfy: ${result.unqfy}, Loggly: ${result.loggly}, Notification: ${result.notification}`)
+    }
+  }
+  )});
 
 router.get("/services/", function(req, res) {
     let result = {
@@ -40,9 +61,23 @@ router.get("/services/", function(req, res) {
       result.loggly = ress[1]
       result.notification = ress[2]
       res.status(200).json(result)
+      slackClient.postMessage(`Unqyfy: ${result.unqfy}, Loggly: ${result.loggly}, Notification: ${result.notification}`)
     })
     
   });
+
+  router.post("/activate", function(req, res){
+    task.start()
+    res.status(204).json("Ok")
+  });
+
+  router.post("/deactivate", function(req, res){
+    console.log("llegue deactivate")
+    task.stop()
+    res.status(204).json("Ok")
+  });
+
+
 
 
   app.use("/api", router);
